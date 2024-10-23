fd03b83596e372eceb07e9264b8bf98e7d35f7a1
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-10-23 11:10:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def get_ga_summary_data():
             "activeUsers": row.metric_values[1].value,
             "bounceRate": row.metric_values[2].value
         })
+    st.write(response)
     return pd.DataFrame(rows)
 
 # Fetch and display Google Analytics data
