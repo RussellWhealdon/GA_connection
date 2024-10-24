9713c0e5f1777a00582d15cea07df3c0d80d5e2c
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-10-23 23:37:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_ga_summary_data():
     request = RunReportRequest(
         property=f"properties/{property_id}",
         metrics=[Metric(name="activeUsers")],
-        date_ranges=[DateRange(start_date="2024-09-01", end_date="2024-09-31")],
+        date_ranges=[DateRange(start_date="2024-09-01", end_date="2024-09-30")],
     )
     
     response = client.run_report(request)
