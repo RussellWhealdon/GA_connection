ec0ccef154081ad58a0b1dbc44a480a981e0cdaf
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-10-24 17:00:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_ga_summary_data():
         property=f"properties/{property_id}",
         dimensions=[
             Dimension(name="date"),
-            Dimension(name="city"),
+            #Dimension(name="city"),
             Dimension(name="source")
         ],  # Add 'date' as a dimension
         metrics=[
@@ -35,7 +35,7 @@ def get_ga_summary_data():
     for row in response.rows:
         # Extract values using dot notation
         date = row.dimension_values[0].value  # Extract the date
-        city = row.dimension_values[1].value  # Extract the city
+        #city = row.dimension_values[1].value  # Extract the city
         source = row.dimension_values[2].value  # Extract the source
         
         active_users = row.metric_values[0].value  # Extract activeUsers
@@ -43,10 +43,10 @@ def get_ga_summary_data():
         bounce_rate = row.metric_values[2].value  # Extract bounceRate
 
         # Append row data to the list
-        rows.append([date, city, source, active_users, sessions, bounce_rate])
+        rows.append([date, source, active_users, sessions, bounce_rate])
     
     # Create a DataFrame with appropriate column names
-    df = pd.DataFrame(rows, columns=['Date', 'City', 'Source', 'Active Users', 'Sessions', 'Bounce Rate'])
+    df = pd.DataFrame(rows, columns=['Date', 'Source', 'Active Users', 'Sessions', 'Bounce Rate'])
 
     return df
 
